a51a10fb2fc0f3854772db41fb41b5b4634d26c3
samys003 <samara_rolim010@hotmail.com>
2025-11-16 19:03:52 -0300

    api dog finalizada

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
  'use strict'
 
-async function buscarImagens () {
-    const url = `https://dog.ceo/api/breed/hound/images`
+async function buscarImagens (dog) {
+    const url = `https://dog.ceo/api/breed/${dog}/images`
     //await e async, são utilizados quando precisamos aguardar uma resposta
     const response = await fetch(url)
     const imagens = await response.json()
     //retorno de imagens dentro do array com a "mensagem" de todos os itens presentes no json
     return imagens.message
+
+  
 }
 
 
-function mostrarResposta(dog){
-
+async function criandoElementos(raca){
     const dogs = document.getElementById('dogs')
-    const label = document.createElement('div')
-    const input = document.createElement('input')
-    const imagens = document.createElement('img')
-    
 
-    dogs.appendChild(input)
-    dogs.appendChild(imagens)
-    label.appendChild(input)
+    dogs.replaceChildren() 
+   
+    raca.forEach( url => {
+    
+    
+    
+    const label = document.createElement('div')
+    const imagens = document.createElement('img')
+ 
+    
+    imagens.src = url
+    label.appendChild(imagens)
+    label.classList.add('label')
+
+   dogs.appendChild(label)
+
+    })
+    
+  
+
+}
+
+async function input() {
+    
+    const input = document.getElementById('input').value
+
+
+    const resposta = await buscarImagens(input)
+    
+  
+
+    criandoElementos(resposta)
 
 }
 
 
-buscarImagens()
+
+const botao = document.getElementById('botao')
+botao.addEventListener('click' ,  input)
